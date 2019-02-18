5756a552569d624a2cc8c3e295ef40a5c170ba10
Bouwman, Gies <gies.bouwman@alliander.com>
2019-02-18 08:11:47 +0100

    alles

---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoSlide
+{
+    public partial class Form1 : Form
+    {
+        Form2 _pForm = new Form2();
+        List<string> _names = null;
+        int _nameIdx = 0;
+        Timer _timer = new Timer();
+
+        const int INTERVAL = 10; // seconden
+        const string PORTRET  = @"C:\Data\Prive\Gerda\DSC_9321.JPG";
+        const string PORTRET_DBG  = @"C:\Data\Prive\Gerda\DSC_6285.JPG";
+        const string DEFAULT_DIRECTORY = @"C:\Data\Prive\Gerda";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _timer.Interval = INTERVAL * 1000;
+            _timer.Tick += _timer_Tick;
+            _pForm.Parent = this;
+        }
+
+        public void StopTimer()
+        {
+            _timer.Stop();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            if (_names == null || _names.Count == 0)
+            {
+                return;
+            }
+            
+            ShowImage(_names[_nameIdx]);
+            do
+            {
+                _nameIdx = (_nameIdx + 1) % _names.Count;
+            } while (_names[_nameIdx] == PORTRET);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _timer.Stop();
+            string s = (File.Exists(PORTRET)) ? PORTRET : _names[0];
+            ShowImage(s);
+        }
+
+        private void ShowImage(string imgpath)
+        {
+            if (!_pForm.Visible)
+            {
+                if (_pForm.IsDisposed)
+                {
+                    _pForm = new Form2();
+                    _pForm.Parent = this;
+                }
+                _pForm.Show();
+                _pForm.WindowState = FormWindowState.Maximized;
+            }
+            _pForm.SetImage(imgpath);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists(DEFAULT_DIRECTORY))
+            {
+                folderBrowserDialog1.SelectedPath = DEFAULT_DIRECTORY;
+            } else
+            {
+                folderBrowserDialog1.SelectedPath = Environment.GetFolderPath(folderBrowserDialog1.RootFolder);
+            }
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+            _names = LoadRandomPaths();
+        }
+
+        private List<string> LoadRandomPaths()
+        {
+            DirectoryInfo di = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            var names = di.GetFiles("*.jpg").Select(fi => fi.FullName).ToList();
+            names.Shuffle();
+            return names;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _timer.Stop();
+            _timer.Start();
+            _timer_Tick(null, null);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+            _names = LoadRandomPaths();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoSlide
+{
+    public partial class Form2 : Form
+    {
+        string _currentPath = String.Empty;
+        public Form1 Parent { get; set; }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void SetImage(string path)
+        {
+            _currentPath = path;
+            ResizeImage();
+        }
+
+        private void ResizeImage()
+        {
+            if (String.IsNullOrEmpty(_currentPath)) {
+                return;
+            }
+            
+            int width = this.pictureBox1.Width;
+            int height = this.pictureBox1.Height;
+            var image = Image.FromFile(_currentPath);
+            image.ExifRotate();
+            var iW = image.Width;
+            var iH = image.Height;
+            double factor = 0;
+            if ((double)iW / iH > Convert.ToDouble(width) / height)
+            {
+                factor = Convert.ToDouble(width) / iW;
+            }
+            else
+            {
+                factor = Convert.ToDouble(height) / iH;
+            }
+            factor = Math.Min(factor, 1D);
+            var tgtWidth = Convert.ToInt32(factor * iW);
+            var tgtHeight = Convert.ToInt32(factor * iH);
+            var bgRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.FillRectangle(Brushes.Black, bgRect);
+                    var destRect = new Rectangle(Convert.ToInt32(Convert.ToDouble(width - tgtWidth) / 2), Convert.ToInt32(Convert.ToDouble(height - tgtHeight) / 2), tgtWidth, tgtHeight);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            this.pictureBox1.Image = destImage;
+            this.label1.Text = _currentPath;
+        }
+
+        private void Form2_Resize(object sender, EventArgs e)
+        {
+            ResizeImage();
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Parent.StopTimer();
+                this.Close();
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                if (this.WindowState == FormWindowState.Maximized)
+                {
+                    this.FormBorderStyle = FormBorderStyle.Sizable;
+                    this.WindowState = FormWindowState.Normal;
+                } else
+                {
+                    this.FormBorderStyle = FormBorderStyle.None;
+                    this.WindowState = FormWindowState.Maximized;
+                }
+            }
+        }
+    }
+}
